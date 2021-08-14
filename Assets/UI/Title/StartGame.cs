db48f99ddff91779449ac7aee53a08d99f350d0a
OhnishiRikito <70005022+plumchloride@users.noreply.github.com>
2021-08-14 19:00:57 +0900

    Add minimal title ui

---
+++ Assets/UI/Title/StartGame.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+  /// ボタンをクリックした時の処理
+  public void OnClick() {
+    SceneManager.LoadScene("move_water");
+  }
+}
