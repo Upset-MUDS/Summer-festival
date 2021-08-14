a7ecc447de91ff3b307dadf2085d05e984c5852d
OhnishiRikito <70005022+plumchloride@users.noreply.github.com>
2021-08-14 13:24:30 +0900

    update move

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     float axisH = 0.0f;
     bool jump = false;
+    public float movementSpeed = 2f;
     void Start()
     {
 
@@ -15,11 +16,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        axisH = Input.GetAxisRaw("Horizontal");
-        jump = Input.GetButtonDown("Jump");
-        if (axisH != 0.0f){
-            Debug.Log(axisH);
-            Debug.Log(jump);
-        }
+        // axisH = Input.GetAxisRaw("Horizontal");
+        // jump = Input.GetButtonDown("Jump");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime,0, verticalInput * movementSpeed * Time.deltaTime);
+        Debug.Log(transform.position);
+        // if (axisH != 0.0f){
+        //     Debug.Log(axisH);
+        //     Debug.Log(jump);
+        // }
     }
 }
