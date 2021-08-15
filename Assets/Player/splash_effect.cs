23a5b8245a500b0404cd3a9970598a7f2aa75b40
sea-alcia <s2022065@stu.musashino-u.ac.jp>
2021-08-15 10:40:31 +0900

    add water splash

--- Assets/Player/splash_effect.cs
+++ Assets/Player/splash_effect.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class splash_effect : MonoBehaviour
 {
-    public GameObject particles; 
+    public GameObject particles1;
+    public GameObject particles2; 
 
     // 水しぶきの設定
     void OnTriggerEnter(Collider t)
@@ -12,7 +13,8 @@ public class splash_effect : MonoBehaviour
         if(t.gameObject.tag == "Water"){
             Debug.Log("sp");
             Vector3 position = t.ClosestPointOnBounds(transform.position);
-            Instantiate(particles, new Vector3(position.x, position.y, position.z), Quaternion.identity);
+            Instantiate(particles1, new Vector3(position.x, position.y + 2, position.z), Quaternion.Euler(90f, 0, 0));
+            Instantiate(particles2, new Vector3(position.x, position.y - 2, position.z), Quaternion.Euler(-90f, 0, 0));
         }
     }
 }
